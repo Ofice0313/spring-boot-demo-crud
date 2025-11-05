f87089020731253784ba66abcd4e3fb83f7b6670
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-11-05 11:42:43 +0200

    Saving the new Student with DAO

--- src/main/java/com/devcaleb/cruddemo/CruddemoApplication.java
+++ src/main/java/com/devcaleb/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.devcaleb.cruddemo;
 
+import com.devcaleb.cruddemo.dao.StudentDAO;
+import com.devcaleb.cruddemo.entities.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +15,24 @@ public class CruddemoApplication{
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student student = new Student("Marcelo", "Ofice", "ofice@gmail.com");
+
+		//save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(student);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + student.getId());
 	}
 
 
---
+++ src/main/java/com/devcaleb/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.devcaleb.cruddemo.dao;
+
+import com.devcaleb.cruddemo.entities.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
